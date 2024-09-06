f930e5b194fb4f9704da4d862fa6113c20046684
Karan0207 <minidox0207@gmail.com>
2024-09-06 22:39:39 +0530

    setup:ngrok

--- src/app/(main)/(pages)/settings/_components/profile-picture.tsx
+++ src/app/(main)/(pages)/settings/_components/profile-picture.tsx
@@ -1,4 +1,8 @@
+"use client"
 import React from 'react'
+import UploadCareButton from './uploadcare-button'
+import { FileUploaderRegular } from '@uploadcare/react-uploader';
+import '@uploadcare/react-uploader/core.css';
 
 type Props = {}
 
@@ -6,6 +10,9 @@ const ProfilePicture = (props: Props) => {
   return (
    <div className="flex flex-col">
     <p className="text-lg text-white">Profile Picture</p>
+    <div className="flex flex-col justify-center items-center h-[30vh]">
+        <UploadCareButton/>
+    </div>
    </div>
   )
 }
