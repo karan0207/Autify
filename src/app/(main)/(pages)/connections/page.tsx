6851df8cdc7004922f7ce4831b95ca01f1736825
Karan0207 <minidox0207@gmail.com>
2024-09-10 22:09:35 +0530

    connection type added in contants

---
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Connections = (props: Props) => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Connections
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -103,3 +103,37 @@ export const clients = [...new Array(10)].map((client, index) => ({
     { name: 'Templates', Component: Templates, href: '/templates' },
     { name: 'Logs', Component: Logs, href: '/logs' },
   ]
+
+
+  export const CONNECTIONS: Connection[] = [
+    {
+      title: 'Google Drive',
+      description: 'Connect your google drive to listen to folder changes',
+      image: '/googleDrive.png',
+      connectionKey: 'googleNode',
+      alwaysTrue: true,
+    },
+    {
+      title: 'Discord',
+      description: 'Connect your discord to send notification and messages',
+      image: '/discord.png',
+      connectionKey: 'discordNode',
+      accessTokenKey: 'webhookURL',
+    },
+    {
+      title: 'Notion',
+      description: 'Create entries in your notion dashboard and automate tasks.',
+      image: '/notion.png',
+      connectionKey: 'notionNode',
+      accessTokenKey: 'accessToken',
+    },
+    {
+      title: 'Slack',
+      description:
+        'Use slack to send notifications to team members through your own custom bot.',
+      image: '/slack.png',
+      connectionKey: 'slackNode',
+      accessTokenKey: 'slackAccessToken',
+      slackSpecial: true,
+    },
+  ]
