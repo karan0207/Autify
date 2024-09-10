c27316832d5d9c638f570a6e1c28b770c1159b5e
Karan0207 <minidox0207@gmail.com>
2024-09-10 22:29:17 +0530

    added:connections higher order fn

---
+++ src/app/(main)/(pages)/connections/_components/connection-card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ConnectionCard = (props: Props) => {
+  return (
+    <div>ConnectionCard</div>
+  )
+}
+
+export default ConnectionCard
--- src/app/(main)/(pages)/connections/page.tsx
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ConnectionCard from "./_components/connection-card";
+import { CONNECTIONS } from "@/lib/constant";
 
 type Props = {
   searchParams?: { [key: string]: string | undefined };
@@ -14,6 +16,14 @@ const Connections = (props: Props) => {
         <section className="flex flex-col gap-4 p-6 text-muted-foreground">
         Connect all your apps directly from here. You may need to connect
         these apps regularly to refresh verification
+        
+        {CONNECTIONS.map((connection)=>{
+             return (
+                <ConnectionCard 
+                key={connection.title} 
+                />
+             )
+        })}
         </section>
       </div>
     </div>
