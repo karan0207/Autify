d7d7e2d77ef6e577da052dde1e5d6f7bbcc1136e
Karan0207 <minidox0207@gmail.com>
2024-09-11 21:37:40 +0530

    connect link added

--- src/app/(main)/(pages)/connections/_components/connection-card.tsx
+++ src/app/(main)/(pages)/connections/_components/connection-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { ConnectionTypes } from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -37,6 +38,28 @@ const ConnectionCard = ({
             <CardDescription>{description}</CardDescription>
         </div>
       </CardHeader>
+      <div className="flex flex-col items-center gap-2 p-4">
+        {/* {connected[type] ? (
+          <div className="border-bg-primary rounded-lg border-2 px-3 py-2 font-bold text-white">
+            Connected
+          </div>
+        ) : ( */}
+          <Link
+            href={
+              title == 'Discord'
+                ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT!
+                : title == 'Notion'
+                ? process.env.NEXT_PUBLIC_NOTION_AUTH_URL!
+                : title == 'Slack'
+                ? process.env.NEXT_PUBLIC_SLACK_REDIRECT!
+                : '#'
+            }
+            className=" rounded-lg bg-primary p-2 font-bold text-primary-foreground"
+          >
+            Connect
+          </Link>
+        {/* )} */}
+      </div>
     </Card>
   );
 };
